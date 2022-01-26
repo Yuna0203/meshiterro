2f07ae456bedffd2d5e2c8d2d750e12f677dd99c
EC2 Default User <ec2-user@ip-172-31-82-51.ec2.internal>
2022-01-26 05:23:36 +0000

    [Finish]4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :post_images, dependent: :destroy
-  has_one_ottached :profile_image
+  has_one_attached :profile_image
 
   def get_profile_image
     unless profile_image_attached?
